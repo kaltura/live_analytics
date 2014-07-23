c2ecd845293330ba4dad704830929eb261842467
Orly Lampert <orly.lampert@kaltura.com>
2014-07-23 10:23:14 +0300

    add live_partner_entry data

--- KalturaLiveModel/src/main/java/com/kaltura/live/model/aggregation/dao/LivePartnerEntryDAO.java
+++ KalturaLiveModel/src/main/java/com/kaltura/live/model/aggregation/dao/LivePartnerEntryDAO.java
@@ -1,15 +1,37 @@
 package com.kaltura.live.model.aggregation.dao;
 
 import java.util.Arrays;
+import java.util.Date;
 import java.util.List;
 
 import com.datastax.driver.core.BoundStatement;
+import com.datastax.driver.core.Row;
 import com.kaltura.live.infra.cache.SerializableSession;
 import com.kaltura.live.model.aggregation.StatsEvent;
 
 public class LivePartnerEntryDAO extends LiveDAO {
 
 	private static final long serialVersionUID = 2179416653993486273L;
+	
+	protected int partnerId;
+	public int getPartnerId() {
+		return partnerId;
+	}
+	
+	protected String entryId;
+	protected Date eventTime;
+	
+	public LivePartnerEntryDAO() {
+		super();
+	}
+	
+	public LivePartnerEntryDAO(Row row) {
+		super(row);
+		this.partnerId = row.getInt("partner_id");
+		this.entryId = row.getString("entry_id");
+		this.eventTime = row.getDate("event_time");
+		
+	}
 
 	@Override
 	protected String getTableName() {
@@ -34,5 +56,26 @@ public class LivePartnerEntryDAO extends LiveDAO {
 	protected List<String> getTableFields() {
 		return Arrays.asList(new String[]{"partner_id", "entry_id", "event_time"});
 	}
+	
+	public void setPartnerId(int partnerId) {
+		this.partnerId = partnerId;
+	}
+
+	public String getEntryId() {
+		return entryId;
+	}
+
+	public void setEntryId(String entryId) {
+		this.entryId = entryId;
+	}
+
+	public Date getEventTime() {
+		return eventTime;
+	}
+
+	public void setEventTime(Date eventTime) {
+		this.eventTime = eventTime;
+	}
+
 
 }
