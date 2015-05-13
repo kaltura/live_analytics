f31fdeab41c43f8bcc7faf75dc76d1980f0fac03
Didi Landau <yehuda.landau@kaltura.com>
2015-05-13 15:20:02 +0300

    test added

--- NewLiveAnalytics/src/test/scala/com/kaltura/Live/model/parse/LiveEventParser$Test.scala
+++ NewLiveAnalytics/src/test/scala/com/kaltura/Live/model/parse/LiveEventParser$Test.scala
@@ -12,28 +12,40 @@ extends FunSuite
 {
      val ipsListFile = "/home/didi/Workspace/Data/ips.txt"
 
-     test("empty ip string")
+//     test("empty ip string")
+//     {
+//          val countryCity = CountryCity.parse("")
+//          assert(countryCity.country==="N/A")
+//          assert(countryCity.city==="N/A")
+//     }
+
+     test("certain problematic ips")
      {
-          val countryCity = CountryCity.parse("")
-          assert(countryCity.country==="N/A")
-          assert(countryCity.city==="N/A")
+          var countryCity = CountryCity.parse("187.207.36.1")
+          println(countryCity.country + ", " + countryCity.city)
+
+          countryCity = CountryCity.parse("189.243.126.161")
+          println(countryCity.country + ", " + countryCity.city)
+
+          countryCity = CountryCity.parse("189.217.142.96")
+          println(countryCity.country + ", " + countryCity.city)
      }
 
-     test("list of ips")
-     {
-          // processing remaining lines
-          for( ipString <- Source.fromFile(ipsListFile).getLines() )
-          {
-               val ipCode = ipString.trim()
-               val countryCity = CountryCity.parse(ipCode)
-               println(countryCity.country + ", " + countryCity.city)
-               assert(countryCity.country!="-")
-               assert(countryCity.city!="-")
-               // split line by comma and process them
-               //l.split(",").map { c =>
-                    // your logic here
-               //}
-          }
-     }
+//     test("list of ips")
+//     {
+//          // processing remaining lines
+//          for( ipString <- Source.fromFile(ipsListFile).getLines() )
+//          {
+//               val ipCode = ipString.trim()
+//               val countryCity = CountryCity.parse(ipCode)
+//               println(countryCity.country + ", " + countryCity.city)
+//               assert(countryCity.country!="-")
+//               assert(countryCity.city!="-")
+//               // split line by comma and process them
+//               //l.split(",").map { c =>
+//                    // your logic here
+//               //}
+//          }
+//     }
 
 }
