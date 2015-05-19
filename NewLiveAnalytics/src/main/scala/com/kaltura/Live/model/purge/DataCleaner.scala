863516162cdc72e3d4947b9ce0084cf56875d54a
Ofir Kerker <ofir.kerker@kaltura.com>
2015-05-19 20:49:28 +0300

    Improve DataCleaner performance

--- NewLiveAnalytics/src/main/scala/com/kaltura/Live/model/purge/DataCleaner.scala
+++ NewLiveAnalytics/src/main/scala/com/kaltura/Live/model/purge/DataCleaner.scala
@@ -2,6 +2,7 @@ package com.kaltura.Live.model.purge
 
 import java.util.Date
 
+import com.kaltura.Live.infra.ConfigurationManager
 import com.kaltura.Live.model.Consts
 import com.kaltura.Live.utils.{BaseLog, MetaLog}
 import org.apache.spark.SparkContext
@@ -15,19 +16,24 @@ import com.datastax.spark.connector.toSparkContextFunctions
 class DataCleaner(sc: SparkContext) extends Serializable with MetaLog[BaseLog] {
 
   var currentIteration = 0
-  var runOnIteration = 200
+  var runOnIteration = ConfigurationManager.get("data_cleaner.num_of_iterations_to_run","20").toInt
 
   def tryRun() {
     currentIteration += 1
     logger.info(s"Current data cleaner iteration is $currentIteration")
     if (currentIteration == runOnIteration) {
-      logger.warn(s"Cleaning data (this is good)!")
-      removeProcessedLogFiles()
-      removeLiveEvents()
-      removeLiveEventsLocation()
+      run()
       currentIteration = 0
-      logger.warn(s"Done cleaning data!")
     }
+  }
+
+  def run() {
+    logger.info(s"Cleaning data (this is good)!")
+    val tStart = System.nanoTime()
+    removeProcessedLogFiles()
+    removeLiveEvents()
+    val tEnd = System.nanoTime()
+    logger.info(s"Done cleaning data! took ${tEnd - tStart}ns")
   }
 
   def runOn(runOnIteration: Int) {
@@ -62,6 +68,7 @@ class DataCleaner(sc: SparkContext) extends Serializable with MetaLog[BaseLog] {
     val dateBefore2Days = new Date(new Date().getTime() - 36 * 3600 * 1000L );
     val liveEventRows = sc.cassandraTable(Consts.KalturaKeySpace, "live_events")
       .where("event_time < ?", dateBefore2Days)
+      .limit(ConfigurationManager.get("data_cleaner.max_num_of_rows_to_delete","1000").toLong)
       .select("entry_id","event_time")
       .as((_: String, _: Long))
     val liveEventsCount = liveEventRows.count()
@@ -72,34 +79,12 @@ class DataCleaner(sc: SparkContext) extends Serializable with MetaLog[BaseLog] {
         session => {
           partition.foreach { row =>
             session.execute(s"DELETE FROM ${Consts.KalturaKeySpace}.live_events where entry_id='${row._1}' and event_time='${row._2}';")
-          }
-        }
-      }
-    })
-    logger.info(s"Deleted $liveEventsCount live_events rows")
-  }
-
-  def removeLiveEventsLocation() {
-
-    val dateBefore2Days = new Date(new Date().getTime() - 36 * 3600 * 1000L );
-    val liveEventLocationRows = sc.cassandraTable(Consts.KalturaKeySpace, "live_events_location")
-      .select("entry_id","event_time")
-      .as((_: String, _: Long))
-      .filter(row => row._2 < dateBefore2Days.getTime)
-
-    val liveEventsCount = liveEventLocationRows.count()
-    logger.info(s"Trying to delete $liveEventsCount live_events_location rows")
-    val connector = CassandraConnector(sc.getConf)
-    liveEventLocationRows.foreachPartition(partition => {
-      connector.withSessionDo {
-        session => {
-          partition.foreach { row =>
             session.execute(s"DELETE FROM ${Consts.KalturaKeySpace}.live_events_location where entry_id='${row._1}' and event_time='${row._2}';")
           }
         }
       }
     })
-    logger.info(s"Deleted $liveEventsCount live_events_location rows")
+    logger.info(s"Deleted $liveEventsCount live_events rows")
   }
 
 }
